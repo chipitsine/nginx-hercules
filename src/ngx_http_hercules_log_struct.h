89f77962d93bd572169c261ad31812fb0d62a509
InHavk <inhavk@gmail.com>
2020-08-25 16:54:39 +0500

    Drop location conf

--- src/ngx_http_hercules_log_struct.h
+++ src/ngx_http_hercules_log_struct.h
@@ -4,16 +4,6 @@
 #include <ngx_core.h>
 #include <ngx_http.h>
 
-/*typedef struct {
-    ngx_buf_t*         buffer;
-    ngx_array_t*       buckets_for_resend;
-    ngx_event_t*       event;
-    ngx_pool_t*        pool;
-    ngx_msec_t         flush;
-    int                socket;
-    ngx_thread_pool_t* thread_pool;
-} ngx_http_hercules_loc_conf_t;*/
-
 typedef struct {
     ngx_buf_t*         buffer;
     ngx_array_t*       buckets_for_resend;
